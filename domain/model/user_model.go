3ad12e4f5bc5edfdacb3d27bcfa42745f0975ddc
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-04-30 21:54:12 +0700

    move to auth model

--- domain/model/user_model.go
+++ domain/model/user_model.go
@@ -5,8 +5,3 @@ type RegisterUser struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
-
-type LoginInput struct {
-	Email    string `json:"email" form:"email" binding:"required,email"`
-	Password string `json:"password" form:"password" binding:"required"`
-}
