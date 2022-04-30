58a21d68d9e5eb0318558c4f45ef1ee67ac6f5cf
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-04-30 23:15:12 +0700

    create auth model

--- domain/model/auth_model.go
+++ domain/model/auth_model.go
@@ -1,6 +1,20 @@
 package model
 
+import "github.com/google/uuid"
+
 type LoginInput struct {
 	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password" form:"password" binding:"required"`
+}
+
+type LoginSuccess struct {
+	UserID uuid.UUID
+	Email  string
+	Token  string
+}
+
+type AuthClaim struct {
+	UserID  uuid.UUID
+	Role    string
+	Expired int64
 }
