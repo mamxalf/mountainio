0e16968bd47aaf5b35671d36617a5168fc8c5813
Hammam Firdaus <hammam.firdaus@mekari.com>
2022-05-02 21:41:01 +0700

    adjust validation

--- validation/user_validation.go
+++ validation/user_validation.go
@@ -2,14 +2,15 @@ package validation
 
 import (
 	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
 	"mountainio/app/exception"
 	"mountainio/domain/model"
 )
 
 func ValidateRegisterUser(params model.RegisterUser) {
 	err := validation.ValidateStruct(&params,
-		validation.Field(&params.Name, validation.Required),
-		validation.Field(&params.Email, validation.Required),
+		validation.Field(&params.Name, validation.Required, validation.Length(4, 150)),
+		validation.Field(&params.Email, validation.Required, is.Email),
 		validation.Field(&params.Password, validation.Required),
 	)
 
